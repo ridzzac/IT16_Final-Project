b1dd3cf0ef7510f890c0714b70873a3a5c0493e8
ridzzac <riddell17soriano@gmail.com>
2025-12-01 12:53:42 +0800

    Add files via upload

--- api/get_incident_list.php
+++ api/get_incident_list.php
@@ -3,10 +3,21 @@
 require __DIR__ . "/../database_connection.php";
 require __DIR__ . "/../util.php";
 
-$start_id = $_GET["start_id"] ?? 0;
-$limit = $_GET["limit"] ?? 20;
-$query = "SELECT * FROM incident WHERE incident_id >= ? LIMIT ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("ii", $start_id, $limit);
-$selectPersonResult = $stmt->execute();
+$incidentPage = [];
+
+if(isset($_GET["incident_page"]) && $_GET["incident_page"] === true) {
+    $start_id = $_GET["start_id"] ?? 0;
+    $limit = $_GET["limit"] ?? 20;
+    $query = "SELECT * FROM incident WHERE incident_id >= ? LIMIT ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ii", $start_id, $limit);
+    $selectPersonResult = $stmt->execute();
+    if($selectPersonResult){
+        $result = $stmt->get_result();
+        while($row = $result->fetch_assoc(MYSQLI_ASSOC)){
+            array_push($incidentPage, $row);
+        }
+    }
+    $stmt->close();
+}
 ?>
--- api/get_people_list.php
+++ api/get_people_list.php
@@ -3,10 +3,21 @@
 require __DIR__ . "/../database_connection.php";
 require __DIR__ . "/../util.php";
 
-$start_id = $_GET["start_id"] ?? 0;
-$limit = $_GET["limit"] ?? 20;
-$query = "SELECT * FROM person WHERE person_id >= ? LIMIT ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("ii", $start_id, $limit);
-$selectPersonResult = $stmt->execute();
+$personPage = [];
+if(isset($_GET["person_page"]) && $_GET["person_page"] === true) {
+    $start_id = $_GET["start_id"] ?? 0;
+    $limit = $_GET["limit"] ?? 20;
+    $query = "SELECT * FROM person WHERE person_id >= ? LIMIT ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ii", $start_id, $limit);
+    $selectPersonResult = $stmt->execute();
+    if($selectPersonResult){
+        $result = $stmt->get_result();
+        while($row = $result->fetch_assoc(MYSQLI_ASSOC)){
+            array_push($personPage, $row);
+        }
+    }
+    $stmt->close;
+}
+
 ?>

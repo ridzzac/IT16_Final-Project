16a4d066e4f7a9de19f3780a1220d457924478e1
ridzzac <riddell17soriano@gmail.com>
2025-12-08 22:42:12 +0800

    Add files via upload

---
+++ upload_person_face.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . "/../database_connection.php";
+const UPLOADS_DIR = "../uploads/";
+
+$isSuccess = false;
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    if($_FILES["uploadedFile"]["error"] != UPLOAD_ERR_OK) {
+        exit;
+    }
+    $person_id = $_POST["person_id"];
+    $fileExtension = pathinfo($_FILES["uploadedFile"]["name"], PATHINFO_EXTENSION);
+    $fileDestination = UPLOADS_DIR . uniqid('', true) . '.' . $fileExtension;
+    $isUploadSuccess = move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $fileDestination);
+
+    if(!$isUploadSuccess)
+        exit;
+
+    $query = "
+        UPDATE person SET face_image_file = ? WHERE person_id = ?
+    ";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("si", $fileDestination, $person_id);
+    $isSuccess = $stmt->execute();
+}
+
+?>

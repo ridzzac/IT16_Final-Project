e4ca1ceb0c50bc285a5dbf39d5992d4124b9673b
ridzzac <riddell17soriano@gmail.com>
2025-12-09 09:43:53 +0800

    Implement user image deletion in delete_user.php
    Added functionality to delete user images before removing the user record.

--- api/delete_user.php
+++ api/delete_user.php
@@ -5,6 +5,21 @@ require __DIR__ . "/../database_connection.php";
 $isSuccess = false;
 if($_SERVER["REQUEST_METHOD"] === "POST"){
     $user_id = $_POST["user_id"];
+
+    // Gets the file path of the image and deletes it
+    $queryForImage = "SELECT face_file_image FROM user WHERE user_id = ?";
+    $stmtForImage = $conn->prepare($query);
+    if($stmtForImage->execute()){
+        $result = $stmtForImage->get_result();
+        $row = $result()->fetch_assoc();
+        $file_path = realpath($row["face_image_file"]);
+        if(file_exists($file_path)){
+            unlink($file_path);
+        }
+            
+    }
+
+    // Delete User Row
     $query = "
         DELETE FROM system_user WHERE user_id = ?
     ";
@@ -13,3 +28,4 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $isSuccess = $stmt->execute();
 }
 ?>
+
